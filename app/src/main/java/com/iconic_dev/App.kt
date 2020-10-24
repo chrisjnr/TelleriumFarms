29dc343097d3b1ebc53c17bd79f56d86c26cf6c9
Manuel Chris-Ogar <manuel.chris-ogar@konga.com>
2020-10-24 21:38:29 +0100

    injected Koin in Application Class

--- app/src/main/java/com/iconic_dev/App.kt
+++ app/src/main/java/com/iconic_dev/App.kt
@@ -1,10 +1,22 @@
 package com.iconic_dev
 
 import android.app.Application
+import com.iconic_dev.telleriumfarms.injection.modules.allRepositoryViewModules
+import com.iconic_dev.telleriumfarms.injection.modules.allViewModelModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
 /**
  * Created by manuelchris-ogar on 24/10/2020.
  */
 class App : Application() {
 
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidContext(this@App)
+            modules(listOf(allRepositoryViewModules, allViewModelModule))
+        }
+    }
 }
