c06f91ef3c79171efe89af273e4dd06ff4c13a68
Manuel Chris-Ogar <manuel.chris-ogar@konga.com>
2020-10-28 11:02:19 +0100

    Moved all functionality  to HomeFragment.kt

--- app/src/main/java/com/iconic_dev/telleriumfarms/MainActivity.kt
+++ app/src/main/java/com/iconic_dev/telleriumfarms/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.iconic_dev.telleriumfarms
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.iconic_dev.telleriumfarms.databinding.ActivityMainBinding
+import com.iconic_dev.telleriumfarms.farmers.api.FarmersAdapter
+import org.koin.android.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
+
+
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater)  }
+    lateinit var adapter: FarmersAdapter
+
+    val viewModel: FarmersViewModel by viewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host) as NavHostFragment?
+        NavigationUI.setupWithNavController(binding.navBar, navHostFragment!!.findNavController())
+
+        binding.navBar.itemIconTintList = null
     }
 }
