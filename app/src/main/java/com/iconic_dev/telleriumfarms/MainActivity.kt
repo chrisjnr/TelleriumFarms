cb10ef85c04e0ec38acacbe7f7535e244abecf50
Manuel Chris-Ogar <manuel.chris-ogar@konga.com>
2020-10-29 20:22:00 +0100

    Code  Cleanup

--- app/src/main/java/com/iconic_dev/telleriumfarms/MainActivity.kt
+++ app/src/main/java/com/iconic_dev/telleriumfarms/MainActivity.kt
@@ -6,26 +6,23 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.iconic_dev.telleriumfarms.databinding.ActivityMainBinding
-import com.iconic_dev.telleriumfarms.farmers.api.FarmersAdapter
-import org.koin.android.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
 
 
     val binding by lazy { ActivityMainBinding.inflate(layoutInflater)  }
-    lateinit var adapter: FarmersAdapter
-
-    val viewModel: FarmersViewModel by viewModel()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+            val navHostFragment = supportFragmentManager
+                .findFragmentById(R.id.nav_host) as NavHostFragment?
+            NavigationUI.setupWithNavController(binding.navBar, navHostFragment!!.findNavController())
 
-        val navHostFragment = supportFragmentManager
-            .findFragmentById(R.id.nav_host) as NavHostFragment?
-        NavigationUI.setupWithNavController(binding.navBar, navHostFragment!!.findNavController())
+            binding.navBar.itemIconTintList = null
 
-        binding.navBar.itemIconTintList = null
+
+
+
     }
 }
