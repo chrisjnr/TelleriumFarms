d35b4c79d9dd0527904525df3dac0813718daa17
Manuel Chris-Ogar <manuel.chris-ogar@konga.com>
2020-10-29 10:52:13 +0100

    Added app wide constants

---
+++ app/src/main/java/com/iconic_dev/telleriumfarms/Constant.java
@@ -0,0 +1,10 @@
+package com.iconic_dev.telleriumfarms;
+
+/**
+ * Created by manuelchris-ogar on 25/10/2020.
+ */
+public class Constant {
+    public final static String LOADING = "Loading";
+    public final static String LOADED = "Loaded";
+    public final static String CHECK_NETWORK_ERROR = "Check your network connection.";
+}
