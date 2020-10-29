c96d47a3cc74e6c54050e8cc78cc486297d63e19
Manuel Chris-Ogar <manuel.chris-ogar@konga.com>
2020-10-29 16:47:12 +0100

    Added Shared Preference Class to handle temporary Saving of user data

---
+++ app/src/main/java/com/iconic_dev/telleriumfarms/api/config/PrefsUser.java
@@ -0,0 +1,27 @@
+package com.iconic_dev.telleriumfarms.api.config;
+
+/**
+ * Created by manuelchris-ogar on 07/09/2020.
+ */
+public class PrefsUser {
+
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
