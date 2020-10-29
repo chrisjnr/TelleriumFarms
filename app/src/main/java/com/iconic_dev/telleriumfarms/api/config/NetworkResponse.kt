2ab3ab0088cacbf56ab5b514501f962bc29f0140
Manuel Chris-Ogar <manuel.chris-ogar@konga.com>
2020-10-29 20:30:51 +0100

    Network classes for retrofit

---
+++ app/src/main/java/com/iconic_dev/telleriumfarms/api/config/NetworkResponse.kt
@@ -0,0 +1,27 @@
+package com.iconic_dev.telleriumfarms.api.config
+
+/**
+ * Created by manuelchris-ogar on 28/06/2020.
+ */
+
+sealed class NetworkResponse<out T : Any, out U : Any> {
+    /**
+     * Success response with body
+     */
+    data class Success<T : Any>(val body: T) : NetworkResponse<T, Nothing>()
+
+    /**
+     * Failure response with body
+     */
+    data class ApiError<U : Any>(val body: U, val code: Int) : NetworkResponse<Nothing, U>()
+
+    /**
+     * Network error
+     */
+    data class NetworkError(val error: Exception) : NetworkResponse<Nothing, Nothing>()
+
+    /**
+     * For example, json parsing error
+     */
+    data class UnknownError(val error: Throwable?) : NetworkResponse<Nothing, Nothing>()
+}
